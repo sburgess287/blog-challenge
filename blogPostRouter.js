c0fba434b902096989d43845877537c9ae63d611
Sarah Burgess <sarahburgess287@gmail.com>
2018-12-05 19:49:48 -0800

    initial commit

---
+++ blogPostRouter.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+const {BlogPosts} = require('./models');
+
+// Add Blog Posts here to get data
+
+// GET
+
+// POST
+
+// DELETE
+
+// PUT
+
+
+
+
+
+
+module.exports = router;
