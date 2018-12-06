2aeb0f571a329a4824b07b8c4ff8926ea5f3a4de
Sarah Burgess <sarahburgess287@gmail.com>
2018-12-05 20:15:22 -0800

    Added GET endpoint to blogPostRouter.js  and removed unnecessary code from server.js

--- blogPostRouter.js
+++ blogPostRouter.js
@@ -4,11 +4,30 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
-const {BlogPosts} = require('./models');
+const {BlogPosts} = require('./model');
 
 // Add Blog Posts here to get data
+BlogPosts.create(
+    'How to Paint a House', 
+    ['Class aptent taciti sociosqu ad litora torquent'], 
+    'Garfield Huxley', 
+    '8/12/97'
+);
+
+BlogPosts.create(
+    'How to Bake a Cake', 
+    ['Mauris condimentum, purus vel euismod bibendum, ipsum arcu pellentesque nunc'], 
+    'Juniper Jones', 
+    '9/12/15'
+);
+
 
 // GET
+router.get('/', (req, res) => {
+    res.json(BlogPosts.get());
+});
+
+
 
 // POST
 
--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 const express = require('express');
-const morgan = require('morgan');
 
 const app = express();
 
 const blogPostRouter = require('./blogPostRouter');
- 
-// log the http layer
-app.use(morgan('common'));
 
 app.use(express.static('public'));
 
 // when requests come into /blogpost, route
 // them to express router instances.
 
-app.use('/blogPostNameTBD', blogPostRouter);
+app.use('/blog-posts', blogPostRouter);
 
 app.listen(process.env.PORT || 8080, () => {
     console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
