05f69cd688bc34631f6c972874f967028180799e
Sarah Burgess <sarahburgess287@gmail.com>
2018-12-12 19:05:31 -0800

    Update to include chai, chai-http, and test configuration

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const morgan = require("morgan");
+//const morgan = require("morgan");
 
 const app = express();
 app.use(express.json());
@@ -7,7 +7,7 @@ app.use(express.json());
 const blogPostRouter = require('./blogPostRouter');
 
 // log the http layer
-app.use(morgan("common"));
+//app.use(morgan("common"));
 
 app.use(express.static('public'));
 
